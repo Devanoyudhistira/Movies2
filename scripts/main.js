03a04f386ade26274f687ca3b4aaaa57705afd46
Devanoyudhistira <devanotira@gmail.com>
2024-02-25 22:02:04 +0700

    finishing title dan membuat main

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
 const slide = document.querySelectorAll('.slide');
-
+const scrollbutton = document.querySelector('.scrollButton');
+const main = document.querySelector('main')
 slide.forEach((elem, idx) => {
     elem.style.transform = `translateX(${idx * 100}%)`;   
 });
-
+scrollbutton.addEventListener('click',() =>{
+    window.scrollTo({
+  top: 700,
+  left: 0,
+  behavior: "smooth",
+});
+    
+})
 
 let curslide = 0;
 
@@ -15,5 +23,4 @@ nextSlide[i].addEventListener("click",function() {
     slide.forEach((slide, indx) => {
         slide.style.transform = `translateX(${100 * (indx - curslide)}%)`;
     })
-    
 })}
