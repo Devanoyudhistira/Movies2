46afb36d2834502e8c6da5fdce3a83623af98c23
Devanoyudhistira <devanotira@gmail.com>
2024-03-01 21:06:35 +0700

    memberi gambar tambahan dan membuat carousel

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const slide = document.querySelectorAll('.slide');
+const nextSlide = document.querySelectorAll(".next");
 const scrollbutton = document.querySelector('.scrollButton');
 const landing = document.querySelector('.landing');
 const main = document.querySelector('main');
@@ -7,6 +8,12 @@ const body = document.querySelector('body');
 slide.forEach((elem, idx) => {
     elem.style.transform = `translateX(${idx * 100}%)`;   
 });
+ 
+
+scrollbutton.addEventListener('click',() =>{
+    body.classList.remove('overflow-hidden')
+})
+
 scrollbutton.addEventListener('click',() =>{
     window.scrollTo({
   top: 10000,
@@ -16,16 +23,11 @@ scrollbutton.addEventListener('click',() =>{
 })
 
 scrollbutton.addEventListener('click',() =>{
-    body.classList.remove('overflow-hidden')
-})
-
-scrollbutton.addEventListener('click',() =>{
     setTimeout(() => {landing.style.display ='none'},700)
 })
 
 let curslide = 0;
 
-const nextSlide = document.querySelectorAll(".next");
 for(let i = 0;i < nextSlide.length;i++){
 nextSlide[i].addEventListener("click",function() {
     curslide++;
@@ -34,8 +36,3 @@ nextSlide[i].addEventListener("click",function() {
         slide.style.transform = `translateX(${100 * (indx - curslide)}%)`;
     })
 })};
-
-const cegah = function(event){
-  event.preventDefault()
-};
-cegah
