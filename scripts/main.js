db3b4409f87ff06f52153db48be89c13e227b5f8
Devanoyudhistira <devanotira@gmail.com>
2024-03-25 10:29:38 +0700

    membuat sinopsis dan judul pada semua film dan anime

--- scripts/main.js
+++ scripts/main.js
@@ -1,95 +1,80 @@
 const slide = document.querySelectorAll('.slide');
-const nextSlide = document.querySelectorAll(".next");
-const scrollbutton = document.querySelector('.scrollButton');
+const nextSlide = document.querySelectorAll('.next');
+const scrollButton = document.querySelector('.scrollButton');
 const rekomendasi = document.querySelectorAll('.rekomendasi');
 const landing = document.querySelector('.landing');
 const main = document.querySelector('main');
 const body = document.querySelector('body');
-let rekomen = document.querySelectorAll('.rekomen');
-let ganti = document.querySelectorAll('.ganti');
-const blockbuster = document.querySelector('.blockbuster ');
+const rekomen = document.querySelectorAll('.rekomen');
+const ganti = document.querySelectorAll('.ganti');
+const blockbuster = document.querySelector('.blockbuster');
 const anime = document.querySelector('.anime-series');
-const movieButton = blockbuster.querySelectorAll('button')
-const movieImg = blockbuster.querySelectorAll('img')
-const animeImg = anime.querySelectorAll('img')
-const animeButton = anime.querySelectorAll('button')
-const thumbnail = document.querySelector('.thumbnail')
-const animethumb = document.querySelector('.anime')
-const close = thumbnail.querySelector('.close')
-const photo = document.querySelector('.photo')
-const watch = document.querySelector('.watch')
-const deskripsi = document.querySelector('.deskripsi')
-const deskripsianime = document.querySelector('.deskripsi-anim')
-const judul = document.querySelectorAll('.judul')
-const animephoto = document.querySelector('.photo-anim')
-const judulanime = document.querySelector('.judulanime')
-const sinopsisanime = document.querySelector('.sinopsisanime')
-const stream = document.querySelector('.stream')
-const closeanime = document.querySelector('.close-anime')
-
+const movieButton = blockbuster.querySelectorAll('button');
+const movieImg = blockbuster.querySelectorAll('img');
+const animeImg = anime.querySelectorAll('img');
+const animeButton = anime.querySelectorAll('button');
+const thumbnail = document.querySelector('.thumbnail');
+const animethumb = document.querySelector('.anime');
+const close = thumbnail.querySelector('.close');
+const photo = document.querySelector('.photo');
+const watch = document.querySelector('.watch');
+const deskripsi = document.querySelector('.deskripsi');
+const deskripsiAnime = document.querySelector('.deskripsi-anim');
+const judul = document.querySelectorAll('.judul');
+const animePhoto = document.querySelector('.photo-anim');
+const judulAnime = document.querySelector('.judulanime');
+const sinopsisAnime = document.querySelector('.sinopsisanime');
+const stream = document.querySelector('.stream');
+const closeAnime = document.querySelector('.close-anime');
+const penghalang = document.querySelector('.penghalang');
 
 slide.forEach((elem, idx) => {
-    elem.style.transform = `translateX(${idx * 100}%)`;   
+    elem.style.transform = `translateX(${idx * 100}%)`;
 });
- 
-main.style.display = 'none'
-scrollbutton.addEventListener('click',() =>{
-    body.classList.remove('overflow-hidden')
-    main.style.display = 'flex'
-})
 
-scrollbutton.addEventListener('click',() =>{
-    window.scrollTo({
-  top:window.innerHeight,
-  left: 0,
-  behavior: "smooth",
+main.style.display = 'none';
+scrollButton.addEventListener('click', () => {
+    body.classList.remove('overflow-hidden');
+    main.style.display = 'flex';
+    window.scrollTo({ top: window.innerHeight, left: 0, behavior: 'smooth' });
+    setTimeout(() => { landing.style.display = 'none'; }, 700);
 });
-})
 
-scrollbutton.addEventListener('click',() =>{
-    setTimeout(() => {landing.style.display ='none'},700)
-})
+let curSlide = 0;
 
-let curslide = 0;
-
-for(let i = 0; i < nextSlide.length; i++) {
-    nextSlide[i].addEventListener("click", function() {
-        curslide++;
-
+for (let i = 0; i < nextSlide.length; i++) {
+    nextSlide[i].addEventListener('click', function () {
+        curSlide++;
         slide.forEach((slide, indx) => {
             setTimeout(() => {
-                slide.style.transform = `translateX(${100 * (indx - curslide)}%)`;
-            }, 10 * indx); // Adjust the delay time (in milliseconds) as needed
+                slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+            }, 10 * indx);
         });
     });
 }
 
+let index = 0;
 
-let index = 0
-
-
-let slide2 = function(n){
+function slideDisplay(n) {
     rekomen.forEach((element) => {
-    element.style.display= 'none'
-});
-    rekomen[n].style.display = 'block'
+        element.style.display = 'none';
+    });
+    rekomen[n].style.display = 'block';
 }
-let control = function(n){
-    slide2(index = n)
+
+function controlSlide(n) {
+    slideDisplay(index = n);
 }
-slide2(index);
 
+slideDisplay(index);
 
-
-close.addEventListener('click',() => {
-    photo.classList.add('-translate-x-[110%]')
-    document.querySelector('.penghalang').style.display = 'none'
+close.addEventListener('click', () => {
+    photo.classList.add('-translate-x-[110%]');
+    penghalang.style.display = 'none';
     watch.classList.add('-translate-x-[110%]');
     deskripsi.classList.add('-translate-x-[110%]');
-    setTimeout(() => {
-    thumbnail.style.transform = 'scalex(0)'
-    },500)
-})
+    setTimeout(() => { thumbnail.style.transform = 'scalex(0)'; }, 500);
+});
 
 function scrollToElement(elementSelector, offset) {
     const element = document.querySelector(elementSelector);
@@ -99,59 +84,47 @@ function scrollToElement(elementSelector, offset) {
     }
 }
 
-thumbnail.style.transform = 'scalex(0)'
-animethumb.style.transform = 'scalex(0)'
+thumbnail.style.transform = 'scalex(0)';
+animethumb.style.transform = 'scalex(0)';
+penghalang.style.display = 'none';
 
-document.querySelector('.penghalang').style.display = 'none'
-
-for(let i = 0;i < movieButton.length;i++){
-    movieButton[i].addEventListener(
-    'click',() =>{
-        scrollToElement('.thumbnail',30)
-        document.querySelector('.penghalang').style.display = 'block'
-        thumbnail.style.transform = 'scalex(1)'
+for (let i = 0; i < movieButton.length; i++) {
+    movieButton[i].addEventListener('click', () => {
+        scrollToElement('.thumbnail', 30);
+        penghalang.style.display = 'block';
+        thumbnail.style.transform = 'scalex(1)';
         photo.src = movieImg[i].src;
+        setTimeout(() => { photo.classList.remove('-translate-x-[110%]'); }, 500);
         setTimeout(() => {
-        photo.classList.remove('-translate-x-[110%]')
-            }, 500);
-        setTimeout(() => {
-        watch.classList.remove('-translate-x-[110%]');
-    deskripsi.classList.remove('-translate-x-[110%]');
+            watch.classList.remove('-translate-x-[110%]');
+            deskripsi.classList.remove('-translate-x-[110%]');
         }, 700);
-        document.querySelector('.sinopsisfilm').textContent = document.querySelectorAll('.sinopsis')[i].textContent
-        const judulfilm = document.querySelector('.judulfilm') 
-        judulfilm.textContent = judul[i].textContent
-    })}
+        document.querySelector('.sinopsisfilm').textContent = document.querySelectorAll('.sinopsis')[i].textContent;
+        const judulfilm = document.querySelector('.judulfilm');
+        judulfilm.textContent = judul[i].textContent;
+    });
+}
 
-
-
-   for(let i = 0;i < animeButton.length;i++){
-    animeButton[i].addEventListener(
-    'click',() =>{
-        scrollToElement('.anime',30)
-        document.querySelector('.penghalang').style.display = 'block'
-        animethumb.style.transform = 'scalex(1)'
-        animephoto.src = animeImg[i].src;
+for (let i = 0; i < animeButton.length; i++) {
+    animeButton[i].addEventListener('click', () => {
+        scrollToElement('.anime', 30);
+        penghalang.style.display = 'block';
+        animethumb.style.transform = 'scalex(1)';
+        animePhoto.src = animeImg[i].src;
+        setTimeout(() => { animePhoto.classList.remove('-translate-x-[160%]'); }, 500);
         setTimeout(() => {
-        animephoto.classList.remove('-translate-x-[160%]')
-            }, 500);
-        setTimeout(() => {
-        stream.classList.remove('-translate-x-[110%]');
-    deskripsianime.classList.remove('-translate-x-[110%]');
+            stream.classList.remove('-translate-x-[110%]');
+            deskripsiAnime.classList.remove('-translate-x-[110%]');
         }, 700);
-        //document.querySelector('.sinopsisfilm').textContent = document.querySelectorAll('.sinopsis')[i].textContent
-        //const judulfilm = document.querySelector('.judulfilm') 
-        //judulfilm.textContent = judul[i].textContent
-    })}
-closeanime.addEventListener('click',() => {
-    animephoto.classList.add('-translate-x-[160%]')
-    document.querySelector('.penghalang').style.display = 'none'
+        sinopsisAnime.textContent = document.querySelectorAll('.synopsis')[i].textContent;
+        judulAnime.textContent = document.querySelectorAll('.title')[i].textContent;
+    });
+}
+
+closeAnime.addEventListener('click', () => {
+    animePhoto.classList.add('-translate-x-[160%]');
+    penghalang.style.display = 'none';
     stream.classList.add('-translate-x-[110%]');
-    deskripsianime.classList.add('-translate-x-[110%]');
-    setTimeout(() => {
-    animethumb.style.transform = 'scalex(0)'
-    },500)
-})
-
-
-
+    deskripsiAnime.classList.add('-translate-x-[110%]');
+    setTimeout(() => { animethumb.style.transform = 'scalex(0)'; }, 500);
+});
